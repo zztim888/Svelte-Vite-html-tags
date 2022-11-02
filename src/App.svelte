93709772b14c547086c34426a46150fa1b42b397
Otim <53155760+zztim888@users.noreply.github.com>
2022-11-02 15:44:22 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+ let string = `this string contains some <strong>hmtl</strong>`;
+</script>
+<!--{@html }you manually escape HTML that comes from sources you don't trust-->
+<p>{@html string}</p>
+
+<style>
+ 
+</style>
